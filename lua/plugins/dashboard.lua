e4ecfd8584d7e02591ef85ed8ab332cef27c721b
ymwm <yingmanwumen@foxmail.com>
2024-09-08 01:05:04 +0800

    feat: configure dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -4,7 +4,39 @@ local this = {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     require('dashboard').setup({
-
+      theme         = "hyper",
+      disable_move  = true,
+      shortcut_type = "number",
+      config        = {
+        week_header = { enable = true },
+        project     = { enable = true },
+        shortcut    = {
+          {
+            icon    = "🔧",
+            icon_hl = "@variable",
+            desc    = "Lazy",
+            group   = "Label",
+            action  = "Lazy",
+            key     = "L",
+          },
+          {
+            icon    = "📚",
+            icon_hl = "@variable",
+            desc    = "Mason",
+            group   = "Label",
+            action  = "Mason",
+            key     = "M",
+          },
+        },
+        mru         = { limit = 20 },
+        hide        = {
+          statusline = true,
+          tabline    = true,
+        },
+        footer      = function()
+          return {}
+        end
+      }
     })
   end
 }
