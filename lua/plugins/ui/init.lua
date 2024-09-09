5655e5c656b32714c42312ee6dfc436941f1811a
ymwm <yingmanwumen@foxmail.com>
2024-09-09 23:09:09 +0800

    feat: add dressing

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -2,5 +2,6 @@ return {
   require('plugins.ui.dashboard'),
   require('plugins.ui.lualine'),
   require('plugins.ui.bufferline'),
-  require('plugins.ui.noice')
+  require('plugins.ui.noice'),
+  require('plugins.ui.dressing')
 }
