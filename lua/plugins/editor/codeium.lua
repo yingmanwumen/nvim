ea2116ff21facffb97a5e2cd2fd9fab9e655ad9a
ymwm <yingmanwumen@foxmail.com>
2024-10-19 19:52:07 +0800

    feat: optional codeium

--- lua/plugins/editor/codeium.lua
+++ lua/plugins/editor/codeium.lua
@@ -1,19 +1,23 @@
-return {
-  "Exafunction/codeium.vim",
-  event = {
-    "InsertEnter",
-    "BufReadPost",
-  },
-  keys = {
-    { "<M-c>", "<Cmd>call codeium#Clear()<CR>", mode = "i" },
-    {
-      "<tab>",
-      "codeium#Accept()",
-      mode = "i",
-      nowait = true,
-      expr = true,
-      silent = true,
-      script = true,
+if vim.uv.os_uname().sysname == "Darwin" then
+  return {}
+else
+  return {
+    "Exafunction/codeium.vim",
+    event = {
+      "InsertEnter",
+      "BufReadPost",
     },
-  },
-}
+    keys = {
+      { "<M-c>", "<Cmd>call codeium#Clear()<CR>", mode = "i" },
+      {
+        "<tab>",
+        "codeium#Accept()",
+        mode = "i",
+        nowait = true,
+        expr = true,
+        silent = true,
+        script = true,
+      },
+    },
+  }
+end
