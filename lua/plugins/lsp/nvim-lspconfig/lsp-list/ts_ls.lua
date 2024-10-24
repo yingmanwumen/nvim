f78bde78172cd3e3d364b1f69db3fb7b732a51a3
Lin Zhenyun <linzhenyun@ft.tech>
2024-10-24 14:04:47 +0800

    feat: add support for vue

--- lua/plugins/lsp/nvim-lspconfig/lsp-list/ts_ls.lua
+++ lua/plugins/lsp/nvim-lspconfig/lsp-list/ts_ls.lua
@@ -1,6 +1,25 @@
+-- local function get_volar_location()
+--   local home = os.getenv("HOME")
+--   if vim.uv.os_uname().sysname == "Darwin" then
+--     return home .. "/.nvm/versions/node/v20.15.0/lib/node_modules/@volar"
+--   else
+--     return ""
+--   end
+-- end
+
 return {
   lsp = "ts_ls",
   opts = {
+    init_options = {
+      -- plugins = { -- I think this was my breakthrough that made it work
+      --   {
+      --     name = "@vue/typescript-plugin",
+      --     location = get_volar_location(),
+      --     languages = { "vue" },
+      --   },
+      -- },
+    },
+    -- filetypes = { "typescript", "javascript", "vue" },
     settings = {
       javascript = {
         inlayHints = {
--- lua/plugins/lsp/nvim-lspconfig/lsp-list/volar.lua
+++ lua/plugins/lsp/nvim-lspconfig/lsp-list/volar.lua
@@ -1,4 +1,14 @@
 return {
   lsp = "volar",
-  opts = {},
+  opts = {
+    -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+    init_options = {
+      vue = {
+        hybridMode = false,
+      },
+      -- typescript = {
+      --   tsdk = os.getenv("HOME") .. "/.nvm/versions/node/v20.15.0/lib/node_modules/typescript/lib",
+      -- },
+    },
+  },
 }
