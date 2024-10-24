a1edc95383315e819396d5f5096c4b35a0ef219a
Lin Zhenyun <linzhenyun@ft.tech>
2024-10-24 14:22:47 +0800

    feat: add suport for vue

--- lua/plugins/lsp/nvim-lspconfig/lsp-list/volar.lua
+++ lua/plugins/lsp/nvim-lspconfig/lsp-list/volar.lua
@@ -1,14 +1,14 @@
 return {
   lsp = "volar",
   opts = {
-    -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+    -- filetypes = { "typescript", "javascript", "vue" },
     init_options = {
       vue = {
         hybridMode = false,
       },
-      -- typescript = {
-      --   tsdk = os.getenv("HOME") .. "/.nvm/versions/node/v20.15.0/lib/node_modules/typescript/lib",
-      -- },
+      typescript = {
+        tsdk = os.getenv("HOME") .. "/.nvm/versions/node/v20.15.0/lib/node_modules/typescript/lib",
+      },
     },
   },
 }
