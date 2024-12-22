527ca096a5ee99886362c2ef7f087d11f2f06dd5
yingmanwumen <yingmanwumen@foxmail.com>
2024-12-22 22:49:57 +0800

    feat: add coplit chat support

---
+++ lua/plugins/editor/copilot_chat.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or
+      { "nvim-lua/plenary.nvim" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    cmd = {
+      "CopilotChat",
+      "CopilotChatOpen",
+      "CopilotChatClose",
+      "CopilotChatToggle",
+      "CopilotChatStop",
+      "CopilotChatReset",
+      "CopilotChatExplain",
+    },
+  },
+}
--- lua/plugins/lsp/symbol-usage.lua
+++ lua/plugins/lsp/symbol-usage.lua
@@ -81,6 +81,7 @@ end
 return {
   "Wansmer/symbol-usage.nvim",
   ft = "rust,c,cpp,python,typescript,javascript",
-  event = "LspAttach", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+  dependencies = { "neovim/nvim-lspconfig" },
+  -- event = "LspAttach", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
   config = setup,
 }
