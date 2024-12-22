db61a18be6488fed2bee4161ab40740fceff3c7b
yingmanwumen <yingmanwumen@foxmail.com>
2024-12-22 14:23:47 +0800

    feat: switch theme

--- init.lua
+++ init.lua
@@ -9,5 +9,5 @@ if vim.uv.os_uname().sysname == "Darwin" then
   vim.cmd("colorscheme gruvbox-material")
 else
   vim.opt.background = "light"
-  vim.cmd("colorscheme onedark")
+  vim.cmd("colorscheme github_light")
 end
