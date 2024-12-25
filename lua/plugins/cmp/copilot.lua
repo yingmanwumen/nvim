a4b72965c883607d39fdc5a5f1b3e8b2b9b62c07
Lin Zhenyun <linzhenyun@ft.tech>
2024-12-25 13:48:10 +0800

    feat: update configurations for copilot cmp

--- lua/plugins/cmp/copilot.lua
+++ lua/plugins/cmp/copilot.lua
@@ -1,7 +1,10 @@
 return {
   "zbirenbaum/copilot-cmp",
-  event = "InsertEnter",
-  dependencies = "hrsh7th/nvim-cmp",
+  event = { "InsertEnter", "LspAttach" },
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+    "zbirenbaum/copilot.lua",
+  },
   config = function()
     require("copilot_cmp").setup()
   end,
