ac8962a18a04d6de2b0b83fce083ab8ae1f65b9e
Lin Zhenyun <linzhenyun@ft.tech>
2024-12-25 14:57:32 +0800

    fix: cmp errors

--- lua/plugins/editor/guard.lua
+++ lua/plugins/editor/guard.lua
@@ -40,8 +40,8 @@ local function setup()
 
   vim.g.guard_config = {
     fmt_on_save = true,
-    lsp_as_default_formatter = true,
-    save_on_format = true,
+    lsp_as_default_formatter = false,
+    save_on_format = false,
   }
 end
 
