d8bb127b4ef89472a955b41ba304beca5c0aa2e3
yingmanwumen <yingmanwumen@foxmail.com>
2025-02-06 22:17:53 +0800

    feat(lsp): enhance diagnostic messages with location info
    Customize LSP diagnostic handler to include file locations in messages.
    Adds file name, line and column numbers to related diagnostic information
    for better error tracing and debugging experience.

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -6,4 +6,25 @@ vim.diagnostic.config({
   },
 })
 
+local original = vim.lsp.handlers["textDocument/publishDiagnostics"]
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+  vim.tbl_map(function(item)
+    if item.relatedInformation and #item.relatedInformation > 0 then
+      vim.tbl_map(function(k)
+        if k.location then
+          local tail = vim.fn.fnamemodify(vim.uri_to_fname(k.location.uri), ":t")
+          k.message = tail
+            .. "("
+            .. (k.location.range.start.line + 1)
+            .. ", "
+            .. (k.location.range.start.character + 1)
+            .. "): "
+            .. k.message
+        end
+        item.message = item.message .. "\n" .. k.message
+      end, item.relatedInformation)
+    end
+  end, result.diagnostics)
+  original(_, result, ctx, config)
+end
 return modules
