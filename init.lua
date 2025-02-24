d36be28072a5a7ef97a9a34306d12261df83e76b
yingmanwumen <yingmanwumen@foxmail.com>
2025-02-24 22:48:07 +0800

    style(theme): switch to onedark theme for non-Darwin systems

--- init.lua
+++ init.lua
@@ -9,5 +9,5 @@ if vim.uv.os_uname().sysname == "Darwin" then
   vim.cmd("colorscheme dayfox")
 else
   vim.opt.background = "dark"
-  vim.cmd("colorscheme nightfox")
+  vim.cmd("colorscheme onedark")
 end
