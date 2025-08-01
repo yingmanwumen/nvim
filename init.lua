9c2f7899b589d91eceb448b27bf1e55cfa164469
Zhen <yingmanwumen@foxmail.com>
2025-08-01 09:31:51 +0800

    feat(nvim-config): Update macOS colorscheme to github_light
    Changes the default light colorscheme for macOS from  to .

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ require("lazy_nvim").setup()
 
 if vim.uv.os_uname().sysname == "Darwin" then
   vim.opt.background = "light"
-  vim.cmd("colorscheme dayfox")
+  vim.cmd("colorscheme github_light")
 else
   vim.opt.background = "dark"
   vim.cmd("colorscheme onedark")
