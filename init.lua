bbdcab2d266fb1888d7fa8cd890dc3b694494b14
yingmanwumen <yingmanwumen@foxmail.com>
2025-09-26 00:50:34 +0800

    feat(nvim-config): Use github_dark colorscheme on Linux
    Switches the default Neovim colorscheme for Linux systems from
    'github_light' to 'github_dark' and sets the background option to 'dark'.
    This provides a more consistent dark theme experience.

--- init.lua
+++ init.lua
@@ -8,8 +8,8 @@ if vim.uv.os_uname().sysname == "Darwin" then
   vim.opt.background = "light"
   vim.cmd("colorscheme github_light")
 else
-  -- vim.opt.background = "dark"
-  -- vim.cmd("colorscheme onedark")
-  vim.opt.background = "light"
-  vim.cmd("colorscheme github_light")
+  vim.opt.background = "dark"
+  vim.cmd("colorscheme github_dark")
+  -- vim.opt.background = "light"
+  -- vim.cmd("colorscheme github_light")
 end
