5f7db5433ce7128d07cca4b0f950ba7201ae1916
Zhen <yingmanwumen@foxmail.com>
2026-01-04 10:49:23 +0800

    refactor(markdown): remove markdown-specific cmp completion setup
    Removed the custom cmp completion sources for markdown files. Markdown buffers now use the global cmp configuration. This simplifies the setup but removes markdown-specific sources such as nvim_lsp, buffer, cmp_tabnine, path, and nerdfont.

--- lua/plugins/languages/markdown/init.lua
+++ lua/plugins/languages/markdown/init.lua
@@ -1,21 +1,8 @@
 local misc = require("misc")
 
-local setup_cmp = function()
-  require("cmp").setup.filetype("markdown", {
-    sources = {
-      { name = "nvim_lsp" },
-      { name = "buffer" },
-      { name = "cmp_tabnine" },
-      { name = "path" },
-      { name = "nerdfont" },
-    },
-  })
-end
-
 local function setup()
   vim.cmd([[setlocal shiftwidth=2]])
   misc.autosave()
-  setup_cmp()
   -- vim.wo.number = false
   -- vim.wo.conceallevel = 3
   -- vim.wo.concealcursor = ""
