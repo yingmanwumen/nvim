9d41905b7552783412713dcc03011b2c5270af4a
yingmanwumen <yingmanwumen@foxmail.com>
2026-07-12 05:43:39 +0800

    fix(ai): avoid XDG runtime writes in Codeium curl

--- lua/plugins/ai/codeium_nvim.lua
+++ lua/plugins/ai/codeium_nvim.lua
@@ -5,6 +5,32 @@ return {
     "hrsh7th/nvim-cmp",
   },
   config = function()
+    local curl = require("plenary.curl")
+    local cache_dir = vim.fn.stdpath("cache") .. "/plenary-curl"
+
+    vim.fn.mkdir(cache_dir, "p", "448")
+
+    local function add_header_dump(opts)
+      opts = opts or {}
+      if opts.dump == nil then
+        opts.dump = {
+          "-D",
+          cache_dir .. "/" .. vim.fn.fnamemodify(vim.fn.tempname(), ":t"),
+        }
+      end
+      return opts
+    end
+
+    local curl_get = curl.get
+    curl.get = function(url, opts)
+      return curl_get(url, add_header_dump(opts))
+    end
+
+    local curl_post = curl.post
+    curl.post = function(url, opts)
+      return curl_post(url, add_header_dump(opts))
+    end
+
     require("codeium").setup({
       enable_cmp_source = true,
       virtual_text = {
